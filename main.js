295895fa5310d287530ddfdd4a4deecc3415963c
mdccg <matheusdanielgomess@gmail.com>
2022-03-08 22:00:46 -0400

    No more Glamrock Freddy

--- main.js
+++ main.js
@@ -1,1 +1,14 @@
-import './style.css'
+const $ = document.querySelector.bind(document);
+
+const addTask = event => {
+  event.preventDefault();
+
+  const taskDescription = $('#task-description').value;
+
+  const newTaskItem = `
+    <li>${taskDescription}</li>
+  `;
+
+  $('#task-list').insertAdjacentHTML('beforeend',  newTaskItem);
+  $('#task-form').reset();
+}
