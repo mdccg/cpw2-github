c4da9e3d55a0bc76bab824e9144825e355b8ec21
Matheus Comparotto <matheusdanielgomess@gmail.com>
2022-03-15 22:07:56 -0400

    Aula sobre local storage

--- main.js
+++ main.js
@@ -1,14 +1,67 @@
 const $ = document.querySelector.bind(document);
 
-const addTask = event => {
-  event.preventDefault();
+const form = $('form');
+const tasksList = $('#lista-de-tarefas');
 
-  const taskDescription = $('#task-description').value;
+function addHtmlTask(taskDescription, checked = false) {
+  const li = `
+    <li>
+      <div>
+        <input
+          type="checkbox"
+          onclick="saveCheckTask('${taskDescription}')"
+          ${checked ? 'checked' : ''} />
+        <span>${taskDescription}</span>
+      </div>
 
-  const newTaskItem = `
-    <li>${taskDescription}</li>
+      <span
+        class="times"
+        onclick="deleteTaskByDescription('${taskDescription}')">&times;</span>
+    </li>
   `;
 
-  $('#task-list').insertAdjacentHTML('beforeend',  newTaskItem);
-  $('#task-form').reset();
+  tasksList.insertAdjacentHTML('beforeend', li);
 }
+
+function getHtmlList() {
+  tasksList.innerHTML = '';
+  console.log(tasks);
+  tasks.forEach(({ taskDescription, checked }) => addHtmlTask(taskDescription, checked));
+}
+
+function setList(tasks) {
+  localStorage.setItem('tasks-list', JSON.stringify(tasks));
+}
+
+function saveTask(taskDescription) {
+  tasks.push({ taskDescription, checked: false });
+  setList(tasks);
+  
+  addHtmlTask(taskDescription);
+}
+
+function saveCheckTask(_taskDescription) {
+  var index = tasks.map(({ taskDescription }) => taskDescription).indexOf(_taskDescription);
+  var task = tasks[index];
+
+  task.checked = !task.checked;
+  tasks[index] = task;
+
+  setList(tasks);
+}
+
+function deleteTaskByDescription(_taskDescription) {
+  tasks = tasks.filter(({ taskDescription }) => taskDescription !== _taskDescription);
+  setList(tasks);
+
+  getHtmlList();
+}
+
+form.addEventListener('submit', function(event) {
+  event.preventDefault();
+  saveTask(event.target.taskDescription.value);
+  form.reset();
+});
+
+var tasks = JSON.parse(localStorage.getItem('tasks-list')) || [];
+getHtmlList();
